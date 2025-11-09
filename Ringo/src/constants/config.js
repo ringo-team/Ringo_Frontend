0f97ec923f6a64c0bb86b57e33a47ac0a231a06a
juminjae <minjae0812@naver.com>
2025-11-09 18:55:05 +0900

    feat: axios 기본 설정 및 api, config 수정

--- Ringo/src/constants/config.js
+++ Ringo/src/constants/config.js
@@ -2,4 +2,30 @@ const BASE_URL = ''; //기본 API URL
 
 const config = {
   API_URL: BASE_URL,
+
+  AUTH: {
+    LOGIN: `${BASE_URL}/login`, //로그인
+    LOGOUT: `${BASE_URL}/logout`, //로그아웃
+    TOKEN: `${BASE_URL}/refresh`, //토큰 갱신(refresh))
+    DELETE: `${BASE_URL}/users/:id`, //회원 탈퇴
+    FIND_ID: `${BASE_URL}/users/find-id`, //아이디 찾기
+    RESET_PASSWORD: `${BASE_URL}/users/reset-password`, //비밀번호 재발급
+  },
+
+  PROFILE: {
+
+  },
+
+  SNAP: {
+
+  },
+
+  SURVEY: {
+
+  },
+
+  MATCH: {
+
+  },
+
 };
